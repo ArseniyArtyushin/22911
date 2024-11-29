4ac78d5bbc131e534cf515b735d5bae4d1f48259
ArsenijArtyushin <arsenijartusin@yandex.ru>
2024-11-29 18:36:26 +0400

    2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ screen.fill((0, 0, 0))
 for i in range(10):
     ew, eh = randint(1, 100), randint(1, 100)
     ex, ey = randint(1, 400), randint(1, 400)
-    pygame.draw.ellipse(screen, (255, 255, 0), (ex, ey, ew, eh))
+    pygame.draw.ellipse(screen, (randint(0, 255), randint(0, 255), randint(0, 255)), (ex, ey, ew, eh))
 pygame.display.flip()
 running = True
 while running:
